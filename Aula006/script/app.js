c59ec5e789a6151c451fafad8827e7a549d2aaff
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-12 21:13:59 -0400

    Ex003 feito!

---
+++ Aula006/script/app.js
@@ -0,0 +1,9 @@
+/*let nome = window.prompt('Qual é o seu nome?');
+window.alert("É um grande prazer te conhecer, " + nome + '!');*/
+
+var n1 = Number(window.prompt('Digite um número: '));
+var n2 = Number(window.prompt('Digite outro número: '));
+var s = n1 + n2;
+window.alert(`A soma entre ${n1} e ${n2} é igual a ${s}`);
+
+
---
+++ Aula007/script/app.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é o seu nome?');
+document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras.`);
+document.write(` Seu nome em maiúsculas é: ${nome.toUpperCase()}`);
+
