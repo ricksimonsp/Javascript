eca2493dc7a412cc0bf8e6b3622f8654e12dea8f
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-15 00:06:25 -0400

    Enough for today! JS

---
+++ Aula009/script/app.js
@@ -0,0 +1,6 @@
+var p1 = window.document.getElementsByTagName('p')[1];
+window.document.write('Está escrito assim: ' + p1.innerText);
+
+var d = window.document.querySelector('div#msg');
+d.style.color = 'green';
+
